5bef37c74003b523c9ea796da68f6e0ef5570831
aibechelllo <aibechelllo@gmail.com>
2023-01-16 22:53:55 +0600

    realy good console Tool for read Int value

---
+++ ConsoleHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace UsefullConsoleTools;
+public class ConsoleHelper
+{
+    public static int ReadIntValue()
+    {
+        do
+        {
+            string value = Console.ReadLine();
+            if (Int32.TryParse(value, out int result))
+                return result;
+        } while (true);
+    }
+}
+
